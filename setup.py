f9b46a47d1a16e279058763670dc1fa0844c55c5
Andrei Matveyeu <andreimu@axis.com>
2024-07-30 08:39:06 +0200

    Remove pyscaffold, add pyproject.toml, setuptools_scm (#36)

--- setup.py
+++ setup.py
@@ -1,23 +1,47 @@
 # -*- coding: utf-8 -*-
-"""Setup file for etos_lib.
-
-Use setup.cfg to configure your project.
-
-This file was generated with PyScaffold 3.2.3.
-PyScaffold helps you to put up the scaffold of your new Python project.
-Learn more under: https://pyscaffold.org/
-"""
-import sys
-
-from pkg_resources import VersionConflict, require
+"""Setup file for etos_lib."""
 from setuptools import setup
+from setuptools_scm.version import get_local_dirty_tag
 
-try:
-    require("setuptools>=38.3")
-except VersionConflict:
-    print("Error: version of setuptools is too old (<38.3)!")
-    sys.exit(1)
+RELEASE_BRANCH = "main"
+
+
+def version_scheme(version) -> str:
+    """Get version component for the current commit.
+
+    Used by setuptools_scm.
+    """
+    if version.branch == RELEASE_BRANCH:
+        if version.distance == 0:
+            # If the current commit is on the release branch and has a GIT tag,
+            # use the tag as version:
+            return f"{version.tag}"
+        else:
+            # For untagged commits always add a distance like ".post3"
+            return f"{version.tag}.post{version.distance}"
+    else:
+        # For non-release branches, make the version as dev and distance:
+        return f"{version.tag}.dev{version.distance}"
+
+
+def local_scheme(version) -> str:
+    """Get local version component for the current Git commit.
+
+    Used by setuptools_scm.
+    """
+    # If current version is dirty, always add dirty tag, regardless of branch.
+    dirty_tag = get_local_dirty_tag(version) if version.dirty else ""
+    if dirty_tag:
+        return f"{dirty_tag}.{version.node}"
+
+    if version.branch == RELEASE_BRANCH:
+        if version.distance == 0:
+            # no local component for versions on the main release branch:
+            # will create simple versions like 4.1.0
+            return ""
+    # For all other cases, always add the git reference (like "g7839952")
+    return f"+{version.node}"
 
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    setup()
